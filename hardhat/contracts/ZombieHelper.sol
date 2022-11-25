e84ffc27f0cdc77d46fcbd4bb86c03b7909b5687
huanggz <918031078@qq.com>
2022-11-25 17:24:20 +0800

    add changeName and changeDna func using modifier

--- hardhat/contracts/ZombieHelper.sol
+++ hardhat/contracts/ZombieHelper.sol
@@ -9,4 +9,22 @@ contract ZombieHelper is ZombieFeeding {
         require(zombies[_zombieId].level >= _level);
         _;
     }
+
+    // changeName change Zombie's name when it's level greater or equal 2
+    function changeName(uint256 _zombieId, string calldata _newName)
+        external
+        aboveLevel(2, _zombieId)
+    {
+        require(zombieToOwner[_zombieId] == msg.sender);
+        zombies[_zombieId].name = _newName;
+    }
+
+    // changeDna change Zombie's dna when it's level greater or equal 20
+    function changeDna(uint256 _zombieId, uint256 calldata _newDna)
+        external
+        aboveLevel(20, _zombieId)
+    {
+        require(zombieToOwner[_zombieId] == msg.sender);
+        zombies[_zombieId].dna = _newDna;
+    }
 }
