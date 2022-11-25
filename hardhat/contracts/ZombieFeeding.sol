a7a3787b47fbaba4fbe386f0938647c0146cfdfa
huanggz <918031078@qq.com>
2022-11-25 14:38:05 +0800

    calcute new dna and create new zombie

--- hardhat/contracts/ZombieFeeding.sol
+++ hardhat/contracts/ZombieFeeding.sol
@@ -5,8 +5,15 @@ import "./ZombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
     // feedAndMultiply _zombieId's zombie feed lifeform's dna
-    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna)
+        public
+        view
+    {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+
+        uint256 newDna = (myZombie.dna + _targetDna) / 2;
+        _createZombie("NoName", newDna);
     }
 }
