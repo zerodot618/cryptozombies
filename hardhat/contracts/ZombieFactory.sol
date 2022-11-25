646b4978c5ac0b81a3fa9f1092e41e2121b019c2
huanggz <918031078@qq.com>
2022-11-25 12:49:13 +0800

    user keccak256 hash func

--- hardhat/contracts/ZombieFactory.sol
+++ hardhat/contracts/ZombieFactory.sol
@@ -26,5 +26,8 @@ contract ZombieFactory {
         private
         view
         returns (uint256)
-    {}
+    {
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
 }
