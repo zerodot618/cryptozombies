6776ce47bcb73bbdbb008d2e464ce14a03a3d6c0
huanggz <918031078@qq.com>
2022-11-25 11:24:05 +0800

    use strcut

--- hardhat/contracts/ZombieFactory.sol
+++ hardhat/contracts/ZombieFactory.sol
@@ -2,6 +2,14 @@
 pragma solidity ^0.8.17;
 
 contract ZombieFactory {
+    // zombie's dna length
     uint256 dnaDigits = 16;
+    // shorten an interger to 16 digits
     uint256 dnaModulus = 10**dnaDigits;
+
+    // zombile struct
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
 }
