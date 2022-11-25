23ea19dbff909253edacaffbd3a03a7c9762e3c9
huanggz <918031078@qq.com>
2022-11-25 14:29:50 +0800

    create feedAndMultiply func

--- hardhat/contracts/ZombieFeeding.sol
+++ hardhat/contracts/ZombieFeeding.sol
@@ -3,4 +3,10 @@ pragma solidity ^0.8.17;
 
 import "./ZombieFactory.sol";
 
-contract ZombieFeeding is ZombieFactory {}
+contract ZombieFeeding is ZombieFactory {
+    // feedAndMultiply _zombieId's zombie feed lifeform's dna
+    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+    }
+}
